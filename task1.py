088840445bcc5bdcb72c7227f2749e2dae9b0c44
victorluizz <victorluizlima1@gmail.com>
2023-04-13 15:11:08 -0300

    Task 1 de Compiladores.

--- task1.py
+++ task1.py
@@ -1,1 +1,36 @@
 
+
+with open('Calc1.stk', "r") as arquivo:
+    pilha = []
+
+    for elemento in arquivo:
+
+        elemento = elemento.rstrip()
+        
+        if elemento.isdigit():
+            pilha.append(int(elemento))
+        else:
+            operador2 = pilha.pop()
+            operador1 = pilha.pop()
+
+            if elemento == "+":
+                resultado = operador1 + operador2
+            elif elemento == "-":
+                resultado = operador1 - operador2
+            elif elemento == "*":
+                resultado = operador1 * operador2
+            elif elemento == "/":
+                resultado = operador1 / operador2
+
+            pilha.append(resultado)
+            resultado_final = resultado
+
+print(resultado_final)
+
+
+
+
+
+
+
+
